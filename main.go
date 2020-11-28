add9bcd729f8ef8dc81da34b140e07b918adb0eb
Radhouen Assakra <askriradhouen@gmail.com>
2020-11-28 12:35:08 +0100

    Update main.go

--- main.go
+++ main.go
@@ -4,7 +4,9 @@ import "fmt"
 func myAdd(x int, y int) int {
 	return x + y
 }
-func dont_use_me(){}
+func dont_use_me(){
+	fmt.Println("Hello 2020")
+}
 func main() {
   i := 5
     fmt.Println("hello world %s", i)
